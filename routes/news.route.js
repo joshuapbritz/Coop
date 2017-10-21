be7ce721984ea4c9652b340bde2dc6dfecc8a2ae
Joshua Britz <joshuabritz@gmail.com>
2017-10-21 16:40:55 +0200

    Moved Contact Link

--- routes/news.route.js
+++ routes/news.route.js
@@ -21,4 +21,11 @@ router.get('/', function(req, res, next) {
     res.render('news', { title: 'Latest News', data: data });
 });
 
+router.get('/read/:id', function(req, res, next) {
+    var page = Number(req.params.id);
+    var article = db.findId(page);
+
+    res.render('view', { title: 'Latest News', data: article });
+});
+
 module.exports = router;
