9d8edfd70bbabaff39861ea2cf276614b1d2d7c3
Joshua Britz <joshuabritz@gmail.com>
2017-10-23 11:45:34 +0200

    Tweaked some styling
    I've tweaked some of the page styling to match the overall theme of the page

--- routes/contact.route.js
+++ routes/contact.route.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 // GET route for reading data
 router.get('/', function(req, res, next) {
-    res.render('contact', { title: 'Contact' });
+    res.render('contact', { title: 'Contact Us' });
+});
+
+router.post('/email', function(req, res, next) {
+    var email = {
+        title: `Contact request from ${req.body.name}`,
+        email: req.body.email,
+        timestamp: new Date().toDateString(),
+        phone: req.body.phone,
+        message: req.body.message,
+    };
+    res.render('email', { layout: 'account', email: email });
 });
 
 module.exports = router;
