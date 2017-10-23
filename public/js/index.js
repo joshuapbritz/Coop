d2241e0a7512ab41ad71aca6e2c8caef6523a2c1
Joshua Britz <joshuabritz@gmail.com>
2017-10-23 16:27:24 +0200

    Changed quote

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-// Add some JavaScript
-document.querySelector('.sidebar-btn').addEventListener('click', function() {
-    var $this = this;
-    var sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('open');
-    $this.classList.toggle('open');
-})
